34de894ac78c1fda9031ab5ae24e2da8ed568389
Eduardo Ismael <eduardo78d@gmail.com>
2016-09-16 13:31:41 -0500

    Simple message

--- index.js
+++ index.js
@@ -16,9 +16,77 @@ app.get('/', function (req, res) {
 
 
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === MY_SECRET_KEY) {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
+  if (req.query['hub.verify_token'] === MY_SECRET_KEY) {
+      res.send(req.query['hub.challenge']);
+  } else {
+      res.send('Invalid verify token');
+  }
 });
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  if (data.object == 'page') {
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+      
+      pageEntry.messaging.forEach(function(messagingEvent) {
+      	if (messagingEvent.message) {
+      		receivedMessage(messagingEvent);
+        } 
+      });
+    });
+
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+  	sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+  callSendAPI(messageData);
+}
+
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+    }
+  });  
+}
+
