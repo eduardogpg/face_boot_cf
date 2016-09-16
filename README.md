d5dc2d48e742823a2b96fbafb9f2e771a9dde5c9
Eduardo Ismael <eduardo78d@gmail.com>
2016-09-16 17:50:16 -0500

    info user

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ##Read me
-
-//https://lit-tor-22939.herokuapp.com/ | https://git.heroku.com/lit-tor-22939.git
-//app_simpson_cf <---app
-//facilito_sim_test <-- Page
+//Last movies Pagina en facebook
+//messenger_app_cf applications
--- index.js
+++ index.js
@@ -2,18 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 const PORT = 3000
 const MY_SECRET_KEY = 'MY_SECRET_KEY'
+const PAGE_ACCESS_TOKEN = 'EAAZALw1vqlscBAIvnudIz6lZAGDhQizpBg6ZBrCWJDYBMZCWK7PgYK2jZCrg5z5rkyPrlsqJ0wXuUx6G7KoxDrRxAad038gtDSuZAi1vI9YZBIMJNPkHXNc2WDbPe9LVaportcVTMrJRZCxVaEmlvMiAmaflAvQbHZCOHYt0XQQ9WdPKv2ZA8v2aTh'
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.listen((process.env.PORT || PORT));
+
+var server = app.listen(PORT, function(){
+  console.log("El servidor se encuentra en el puerto "+ PORT);
+});
 
 app.get('/', function (req, res) {
     res.send('This is a project for my own Facebook bot');
 });
-
 
 app.get('/webhook', function (req, res) {
   if (req.query['hub.verify_token'] === MY_SECRET_KEY) {
@@ -32,9 +36,11 @@ app.post('/webhook', function (req, res) {
       var timeOfEvent = pageEntry.time;
       
       pageEntry.messaging.forEach(function(messagingEvent) {
-      	if (messagingEvent.message) {
-      		receivedMessage(messagingEvent);
-        } 
+
+        if(messagingEvent.message){
+          receivedMessage(messagingEvent);
+        }
+
       });
     });
 
@@ -42,20 +48,38 @@ app.post('/webhook', function (req, res) {
   }
 });
 
+function sendWelcomeMessage(recipientId, userData){
+  var message = "Hola "+ userData.first_name +" te doy la bienvenida a lo que será nuestro nuevo curso aqui en código facilito, espero que sea de tu agrado! ";
+  sendTextMessage(recipientId,message);
+
+  var message = "Si tienes alguna duda, sientete libre en enviar un coreo a eduardo@codigofacilito.com";
+  sendTextMessage(recipientId, message);
+}
+
+
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
+
   var timeOfMessage = event.timestamp;
   var message = event.message;
 
-  var messageId = message.mid;
+  getUser(senderID, sendWelcomeMessage);
+}
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
 
-  if (messageText) {
-  	sendTextMessage(senderID, "Message with attachment received");
-  }
+function getUser(recipientId, functionCallBack){
+  request({
+    uri: 'https://graph.facebook.com/v2.6/'+ recipientId,
+    qs: {access_token: PAGE_ACCESS_TOKEN},
+    method: 'GET'
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      data = JSON.parse(response.body);
+      functionCallBack(recipientId, data)
+    }
+  }); 
 }
 
 function sendTextMessage(recipientId, messageText) {
@@ -74,7 +98,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    qs: {access_token: PAGE_ACCESS_TOKEN},
     method: 'POST',
     json: messageData
 
